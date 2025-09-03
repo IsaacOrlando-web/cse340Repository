e18c3b9daa7f8690d681a5515f828c6eecfa62e5
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-09-03 18:11:50 -0400

    using render

--- server.js
+++ server.js
@@ -6,14 +6,28 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts") //we tell the application to require express-ejs-layouts, so it can be used.
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+//Index route
+app.get("/", function (req, res) {
+  res.render("index", {title: "Home"})
+})
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs") //we declare that ejs will be the view engine for our application
+app.use(expressLayouts) //This two lines tells the application to use the express-ejs-layouts package, which has been stored into a "expressLayouts" variable.
+app.set("layout", "./layouts/layout") //This line says that when the express ejs layout goes looking for the basic template for a view, it will find it in a layouts folder, and the template will be named layout.
+
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static) // meaning that the application itself will use this resource.
 
 /* ***********************
  * Local Server Information
