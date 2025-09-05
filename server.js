a789f4cbe570d20e4a7c7eedf1a2a5a7c0ad06bf
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-09-05 14:28:03 -0400

    layout and index fix it

--- server.js
+++ server.js
@@ -11,18 +11,21 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-//Index route
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"})
-})
-
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs") //we declare that ejs will be the view engine for our application
 app.use(expressLayouts) //This two lines tells the application to use the express-ejs-layouts package, which has been stored into a "expressLayouts" variable.
-app.set("layout", "layouts/layout") //This line says that when the express ejs layout goes looking for the basic template for a view, it will find it in a layouts folder, and the template will be named layout.
+app.set("layout", "./layouts/layout") //This line says that when the express ejs layout goes looking for the basic template for a view, it will find it in a layouts folder, and the template will be named layout.
+app.set("view engine", "ejs") //we declare that ejs will be the view engine for our application
+
+
+
+//Index route
+app.get('', (req, res) => {
+  res.render("index", {title: "Home"})
+})
+
 
 /* ***********************
  * Routes
