882aa51055830c727c54ce6b29926c539ac041de
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-09-05 13:06:43 -0400

    index page ready. Styles Done and Wave review Done

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", function (req, res) {
  *************************/
 app.set("view engine", "ejs") //we declare that ejs will be the view engine for our application
 app.use(expressLayouts) //This two lines tells the application to use the express-ejs-layouts package, which has been stored into a "expressLayouts" variable.
-app.set("layout", "./layouts/layout") //This line says that when the express ejs layout goes looking for the basic template for a view, it will find it in a layouts folder, and the template will be named layout.
+app.set("layout", "layouts/layout") //This line says that when the express ejs layout goes looking for the basic template for a view, it will find it in a layouts folder, and the template will be named layout.
 
 /* ***********************
  * Routes
