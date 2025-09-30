cee662647a71c2d7fafeda169028f45de625805d
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-09-30 11:46:34 -0300

    add category and inventory added

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities")
+const invChecks = require("../utilities/inventory-validation")
 
 // Route to build inventory by classification view
 
@@ -14,6 +15,42 @@ utilities.handleErrors(invController.buildDetail))
 router.get(
     "/broken",
     utilities.handleErrors(invController.thowError)
+)
+
+router.get(
+  "/",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.buildManagementView)
+)
+
+router.get(
+  "/newClassification",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.newClassificationView)
+)
+
+
+router.post(
+  "/addClassification",
+  //utilities.checkAccountType,
+  invChecks.classificationRule(),
+  invChecks.checkClassificationData,
+  utilities.handleErrors(invController.addClassification)
+)
+
+router.get(
+  "/newVehicle",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.newInventoryView)
+)
+
+
+router.post(
+  "/addInventory",
+  //utilities.checkAccountType,
+  invChecks.newInventoryRules(),
+  invChecks.checkInventoryData,
+  utilities.handleErrors(invController.addInventory)
 )
 
 module.exports = router;
--- utilities/index.js
+++ utilities/index.js
@@ -24,6 +24,25 @@ Util.getNav = async function (req, res, next) {
     return list
 }
 
+Util.buildClassificationList = async function (classification_id = null) {
+    let data = await invModel.getClassifications()
+    let classificationList =
+    '<select name="classification_id" id="classificationList" required>'
+    classificationList += "<option value=''>Choose a Classification</option>"
+    data.rows.forEach((row) => {
+        classificationList += '<option value="' + row.classification_id + '"'
+        if (
+            classification_id != null &&
+            row.classification_id == classification_id
+        ) {
+            classificationList += " selected "
+        }
+        classificationList += ">" + row.classification_name + "</option>"
+    })
+    classificationList += "</select>"
+    return classificationList
+}
+
 module.exports = Util
 
 //This file will hold functions that are "utility" in nature, meaning that we will reuse them over and over, but they don't directly belong to the M-V-C structure
